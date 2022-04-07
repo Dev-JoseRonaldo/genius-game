edc1660226ff631584849bee87487e109ea0ac7c
José Ronaldo <dev.joseronaldo@gmail.com>
2022-04-07 12:20:26 -0300

    Organizando o projeto, pagina inicial, pagina de jogo não encontrado provisória, navegação entre paginas.

--- script/maingame.js
+++ script/maingame.js
@@ -3,37 +3,37 @@ var games = [
         "id": "0",
         "name": "Jogo da cobrinha",
         "img": "https://s2.glbimg.com/HDXr8p36WSK9lXo9RDAvMMhSZGw=/0x0:1400x816/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/P/3/gF0mfQTSmAZZXQ41JFPw/jogos-da-cobrinha-gratis-destaque.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "1",
         "name": "Genius",
         "img": "https://s2.glbimg.com/cX5ZbmXzi8cpVk1gJo6u5_l_kaI=/0x600/s.glbimg.com/po/tt2/f/original/2014/09/11/df6043ec012dfef612313b075c91.jpeg",
-        "url": "./games/genius.html"
+        "url": "../games/genius.html"
     },
     {
         "id": "2",
         "name": "Jogo da Velha",
         "img": "https://www.bananaquantica.com.br/wp-content/uploads/2020/12/41ZApWfYLUL.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "3",
         "name": "Space Shot",
         "img": "https://play-lh.googleusercontent.com/VsJBTv7Ta15_SDZbxM0C-GGHhYYx2e1brTFYUx8vDafNCzJrv3mRK2iX2oX8mxb3hgU",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "4",
         "name": "Mario Boss",
         "img": "https://i.ytimg.com/vi/cWOkHQXw0JQ/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "5",
         "name": "Jogo da Memória",
         "img": "https://i.ytimg.com/vi/rvm0q-vNVEI/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "6",
@@ -44,75 +44,75 @@ var games = [
         "id": "7",
         "name": "Stunts",
         "img": "https://i.ytimg.com/vi/-hKK4_gvOS0/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "8",
         "name": "Donkey Kong",
         "img": "https://ucegamers.com.br/siteuceg/wp-content/uploads/2018/07/image1.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "9",
         "name": "Pac-man",
         "img": "https://i.pinimg.com/474x/63/46/7f/63467f56eb0d31c0574b0daf787ffb0b--vintage-games-pac-man.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "10",
         "name": "Sim city",
         "img": "https://www.retrogamer.net/wp-content/uploads/2013/10/snes_sim_city.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "11",
         "name": "Prince of Persia",
         "img": "https://macmagazine.com.br/wp-content/uploads/2010/05/28-prince.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "12",
         "name": "Bust a move",
         "img": "https://gaminghistory101.files.wordpress.com/2015/04/bam2ae_2.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "13",
         "name": "Jogo da cobrinha",
         "img": "https://s2.glbimg.com/HDXr8p36WSK9lXo9RDAvMMhSZGw=/0x0:1400x816/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/P/3/gF0mfQTSmAZZXQ41JFPw/jogos-da-cobrinha-gratis-destaque.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "14",
         "name": "Genius",
         "img": "https://s2.glbimg.com/cX5ZbmXzi8cpVk1gJo6u5_l_kaI=/0x600/s.glbimg.com/po/tt2/f/original/2014/09/11/df6043ec012dfef612313b075c91.jpeg",
-        "url": "./games/genius.html"
+        "url": "../games/genius.html"
     },
     {
         "id": "15",
         "name": "Jogo da Velha",
         "img": "https://www.bananaquantica.com.br/wp-content/uploads/2020/12/41ZApWfYLUL.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "16",
         "name": "Space Shot",
         "img": "https://play-lh.googleusercontent.com/VsJBTv7Ta15_SDZbxM0C-GGHhYYx2e1brTFYUx8vDafNCzJrv3mRK2iX2oX8mxb3hgU",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "17",
         "name": "Mario Boss",
         "img": "https://i.ytimg.com/vi/cWOkHQXw0JQ/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "18",
         "name": "Jogo da Memória",
         "img": "https://i.ytimg.com/vi/rvm0q-vNVEI/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "19",
@@ -123,75 +123,75 @@ var games = [
         "id": "20",
         "name": "Stunts",
         "img": "https://i.ytimg.com/vi/-hKK4_gvOS0/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "21",
         "name": "Donkey Kong",
         "img": "https://ucegamers.com.br/siteuceg/wp-content/uploads/2018/07/image1.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "22",
         "name": "Pac-man",
         "img": "https://i.pinimg.com/474x/63/46/7f/63467f56eb0d31c0574b0daf787ffb0b--vintage-games-pac-man.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "23",
         "name": "Sim city",
         "img": "https://www.retrogamer.net/wp-content/uploads/2013/10/snes_sim_city.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "24",
         "name": "Prince of Persia",
         "img": "https://macmagazine.com.br/wp-content/uploads/2010/05/28-prince.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "25",
         "name": "Bust a move",
         "img": "https://gaminghistory101.files.wordpress.com/2015/04/bam2ae_2.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "26",
         "name": "Jogo da cobrinha",
         "img": "https://s2.glbimg.com/HDXr8p36WSK9lXo9RDAvMMhSZGw=/0x0:1400x816/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/P/3/gF0mfQTSmAZZXQ41JFPw/jogos-da-cobrinha-gratis-destaque.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "27",
         "name": "Genius",
         "img": "https://s2.glbimg.com/cX5ZbmXzi8cpVk1gJo6u5_l_kaI=/0x600/s.glbimg.com/po/tt2/f/original/2014/09/11/df6043ec012dfef612313b075c91.jpeg",
-        "url": "./games/genius.html"
+        "url": "../games/genius.html"
     },
     {
         "id": "28",
         "name": "Jogo da Velha",
         "img": "https://www.bananaquantica.com.br/wp-content/uploads/2020/12/41ZApWfYLUL.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "29",
         "name": "Space Shot",
         "img": "https://play-lh.googleusercontent.com/VsJBTv7Ta15_SDZbxM0C-GGHhYYx2e1brTFYUx8vDafNCzJrv3mRK2iX2oX8mxb3hgU",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "30",
         "name": "Mario Boss",
         "img": "https://i.ytimg.com/vi/cWOkHQXw0JQ/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "31",
         "name": "Jogo da Memória",
         "img": "https://i.ytimg.com/vi/rvm0q-vNVEI/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "32",
@@ -202,75 +202,75 @@ var games = [
         "id": "33",
         "name": "Stunts",
         "img": "https://i.ytimg.com/vi/-hKK4_gvOS0/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "34",
         "name": "Donkey Kong",
         "img": "https://ucegamers.com.br/siteuceg/wp-content/uploads/2018/07/image1.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "35",
         "name": "Pac-man",
         "img": "https://i.pinimg.com/474x/63/46/7f/63467f56eb0d31c0574b0daf787ffb0b--vintage-games-pac-man.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "36",
         "name": "Sim city",
         "img": "https://www.retrogamer.net/wp-content/uploads/2013/10/snes_sim_city.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "37",
         "name": "Prince of Persia",
         "img": "https://macmagazine.com.br/wp-content/uploads/2010/05/28-prince.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "38",
         "name": "Bust a move",
         "img": "https://gaminghistory101.files.wordpress.com/2015/04/bam2ae_2.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "39",
         "name": "Jogo da cobrinha",
         "img": "https://s2.glbimg.com/HDXr8p36WSK9lXo9RDAvMMhSZGw=/0x0:1400x816/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/P/3/gF0mfQTSmAZZXQ41JFPw/jogos-da-cobrinha-gratis-destaque.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "40",
         "name": "Genius",
         "img": "https://s2.glbimg.com/cX5ZbmXzi8cpVk1gJo6u5_l_kaI=/0x600/s.glbimg.com/po/tt2/f/original/2014/09/11/df6043ec012dfef612313b075c91.jpeg",
-        "url": "./games/genius.html"
+        "url": "../games/genius.html"
     },
     {
         "id": "41",
         "name": "Jogo da Velha",
         "img": "https://www.bananaquantica.com.br/wp-content/uploads/2020/12/41ZApWfYLUL.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "42",
         "name": "Space Shot",
         "img": "https://play-lh.googleusercontent.com/VsJBTv7Ta15_SDZbxM0C-GGHhYYx2e1brTFYUx8vDafNCzJrv3mRK2iX2oX8mxb3hgU",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "43",
         "name": "Mario Boss",
         "img": "https://i.ytimg.com/vi/cWOkHQXw0JQ/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "44",
         "name": "Jogo da Memória",
         "img": "https://i.ytimg.com/vi/rvm0q-vNVEI/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "45",
@@ -281,75 +281,75 @@ var games = [
         "id": "46",
         "name": "Stunts",
         "img": "https://i.ytimg.com/vi/-hKK4_gvOS0/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "47",
         "name": "Donkey Kong",
         "img": "https://ucegamers.com.br/siteuceg/wp-content/uploads/2018/07/image1.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "48",
         "name": "Pac-man",
         "img": "https://i.pinimg.com/474x/63/46/7f/63467f56eb0d31c0574b0daf787ffb0b--vintage-games-pac-man.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "49",
         "name": "Sim city",
         "img": "https://www.retrogamer.net/wp-content/uploads/2013/10/snes_sim_city.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "50",
         "name": "Prince of Persia",
         "img": "https://macmagazine.com.br/wp-content/uploads/2010/05/28-prince.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
 
     {
         "id": "51",
         "name": "Bust a move",
         "img": "https://gaminghistory101.files.wordpress.com/2015/04/bam2ae_2.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "52",
         "name": "Jogo da cobrinha",
         "img": "https://s2.glbimg.com/HDXr8p36WSK9lXo9RDAvMMhSZGw=/0x0:1400x816/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2017/P/3/gF0mfQTSmAZZXQ41JFPw/jogos-da-cobrinha-gratis-destaque.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "53",
         "name": "Genius",
         "img": "https://s2.glbimg.com/cX5ZbmXzi8cpVk1gJo6u5_l_kaI=/0x600/s.glbimg.com/po/tt2/f/original/2014/09/11/df6043ec012dfef612313b075c91.jpeg",
-        "url": "./games/genius.html"
+        "url": "../games/genius.html"
     },
     {
         "id": "54",
         "name": "Jogo da Velha",
         "img": "https://www.bananaquantica.com.br/wp-content/uploads/2020/12/41ZApWfYLUL.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "55",
         "name": "Space Shot",
         "img": "https://play-lh.googleusercontent.com/VsJBTv7Ta15_SDZbxM0C-GGHhYYx2e1brTFYUx8vDafNCzJrv3mRK2iX2oX8mxb3hgU",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "56",
         "name": "Mario Boss",
         "img": "https://i.ytimg.com/vi/cWOkHQXw0JQ/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "57",
         "name": "Jogo da Memória",
         "img": "https://i.ytimg.com/vi/rvm0q-vNVEI/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "58",
@@ -360,20 +360,20 @@ var games = [
         "id": "59",
         "name": "Stunts",
         "img": "https://i.ytimg.com/vi/-hKK4_gvOS0/maxresdefault.jpg",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     },
     {
         "id": "60",
         "name": "Donkey Kong",
         "img": "https://ucegamers.com.br/siteuceg/wp-content/uploads/2018/07/image1.png",
-        "url": "./games/404.html"
+        "url": "../games/404.html"
     }
 ];
 
 var row = document.querySelector('#row');
 var current_url = window.location.href;
 console.log(current_url);
-for (let i = 0; i < games.length && i <= 59; i++) {
+for (let i = 0; i < games.length && i <= 11; i++) {
     if (!(games[i].url === current_url)) {
         row.innerHTML += `
         <div class="col-4 col-sm-4 col-md-4 col-lg-3 col-xl-2 mb-3">
